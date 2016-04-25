8d95036c7e421d3a05353bb679f4b6f690bce8d6
MCing <843774017@qq.com>
2016-04-25 21:27:00 +0800

    添加配置数据库菜单接口（未完成）

--- src/com/eqsys/util/SysConfig.java
+++ src/com/eqsys/util/SysConfig.java
@@ -96,4 +96,8 @@ public class SysConfig {
 	public static int getServerPort() {
 		return serverPort;
 	}
+	
+	public void saveDbPro(){
+		
+	}
 }
---
+++ src/com/eqsys/view/DatabaseSettingController.java
@@ -0,0 +1,53 @@
+package com.eqsys.view;
+
+import com.eqsys.util.SysConfig;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class DatabaseSettingController {
+
+	private Stage stage;
+	
+	@FXML
+	private TextField serverName;
+	@FXML
+	private TextField dbName;
+	@FXML
+	private TextField port;
+	@FXML
+	private TextField user;
+	@FXML
+	private PasswordField password;
+	
+	@FXML
+	private void initialize(){
+		serverName.setText(SysConfig.getJdbcServerName());
+		dbName.setText(SysConfig.getJdbcDb());
+		port.setText(String.valueOf(SysConfig.getJdbcPort()));
+		user.setText(SysConfig.getJdbcUser());
+		password.setText(SysConfig.getJdbcPasswd());
+	}
+	
+	public void initController(Stage stage){
+		this.stage = stage;
+	}
+	
+	@FXML
+	private void handleOk(){
+		String jdbcUser = user.getText();
+		String jdbcPasswd = password.getText();
+		String jdbcDb = dbName.getText();
+		String jdbcPort = port.getText();
+		String jdbcServerName = serverName.getText();
+		
+		stage.close();
+	}
+	
+	@FXML 
+	private void handleCancel(){
+		stage.close();
+	}
+}
